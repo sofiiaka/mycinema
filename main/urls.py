974c31283675bf96f3fa328d6977dcd5a46fd746
sofiiaka <kasiyanchuk.sofiya@gmail.com>
2023-01-11 18:08:47 +0200

    add new view, url, template so that the movie is not saved multiple times

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
     path('add-film', views.add_film, name='add_film'),
     path('film/<id>',  views.film_details, name='film_detail'),
     path('add-film-to-collection/<id>', views.add_film_to_collection, name='add_film_to_collection'),
-    path('collections-film/', views.collections_film, name='collections_film')
+    path('collections-film/', views.collections_film, name='collections_film'),
+    path('film-already-exist-error/', views.film_already_exist, name='film_already_exist_error')
 ]
--- main/views.py
+++ main/views.py
@@ -57,41 +57,45 @@ def film_details(request, id):
 def add_film_to_collection(request, id):
     film_details = requests.get(f'https://api.themoviedb.org/3/movie/{id}?api_key={API_KEY_TMDB}').text
     film_details = json.loads(film_details)
-    for production_country in film_details.get('production_countries'):
-        if production_country.get('iso_3166_1') and production_country.get('name'):
-            try:
-                country = Country.objects.get(country_short_name=film_details.get('iso_3166_1'), country_full_name=film_details.get('name'))
-                production_country_id = country.id
-            except Exception:
-                country = Country(
-                    country_short_name=production_country.get('iso_3166_1'),
-                    country_full_name=production_country.get('name')
-                )
-                country.save()
-                production_country_id = country.id
-    for genres in film_details.get('genres'):
-        if genres.get('id') and genres.get('name'):
-            try:
-                genre = Genre.objects.get(id=film_details.get('id'), genre=film_details.get('name'))
-                genre_id = genre.id
-            except Exception:
-                genre = Genre(
-                    id=genres.get('id'),
-                    genre=genres.get('name')
-                )
-                genre.save()
-                genre_id = genre.id
+    film_already_exist = Film.objects.filter(title=film_details.get('title')).exists()
+    if film_already_exist:
+        return redirect('/film-already-exist-error/')
+    else:
+        for production_country in film_details.get('production_countries'):
+            if production_country.get('iso_3166_1') and production_country.get('name'):
+                try:
+                    country = Country.objects.get(country_short_name=film_details.get('iso_3166_1'), country_full_name=film_details.get('name'))
+                    production_country_id = country.id
+                except Exception:
+                    country = Country(
+                        country_short_name=production_country.get('iso_3166_1'),
+                        country_full_name=production_country.get('name')
+                    )
+                    country.save()
+                    production_country_id = country.id
+        for genres in film_details.get('genres'):
+            if genres.get('id') and genres.get('name'):
+                try:
+                    genre = Genre.objects.get(id=film_details.get('id'), genre=film_details.get('name'))
+                    genre_id = genre.id
+                except Exception:
+                    genre = Genre(
+                        id=genres.get('id'),
+                        genre=genres.get('name')
+                    )
+                    genre.save()
+                    genre_id = genre.id
 
-    film = Film(
-        title=film_details.get('original_title'),
-        overview=film_details.get('overview'),
-        duration=film_details.get('runtime'),
-        year_production=film_details.get('release_date'),
-        genre_id=genre,
-        country_id=country
-    )
-    film.save()
-    return redirect('/collections-film/')
+        film = Film(
+            title=film_details.get('original_title'),
+            overview=film_details.get('overview'),
+            duration=film_details.get('runtime'),
+            year_production=film_details.get('release_date'),
+            genre_id=genre,
+            country_id=country
+        )
+        film.save()
+        return redirect('/collections-film/')
 
 
 def collections_film(request):
@@ -100,3 +104,7 @@ def collections_film(request):
         'films': films
     }
     return render(request, 'main/my_films_collection.html', data)
+
+
+def film_already_exist(request):
+    return render(request, 'main/film_already_exist_error.html')
